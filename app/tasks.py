b0794010e47b11af7d2cc57220da96d2be8675fa
anthony <anthony.teixeira55@gmail.com>
2023-12-19 11:03:21 -0500

    stuff

--- app/tasks.py
+++ app/tasks.py
@@ -2,11 +2,14 @@ from flask import current_app
 from .stripe_services import fetch_used_coupons
 from .email_services import send_email
 
+
 def send_report():
     with current_app.app_context():
         used_coupons = fetch_used_coupons()
         if 'error' in used_coupons:
             print(f"Error occurred: {used_coupons['error']}")
+            return
+        if not used_coupons:
             return
         subject = "Used Coupons Report"
         sender = current_app.config['MAIL_USERNAME']
