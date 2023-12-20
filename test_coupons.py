942ac5e397749a9fcd0fc5d3866cf4c23bf9a38b
anthony <anthony.teixeira55@gmail.com>
2023-12-19 21:25:01 -0500

    tests done

--- test_coupons.py
+++ test_coupons.py
@@ -1,10 +1,19 @@
+import unittest
 from app import create_app
 from app.stripe_services import fetch_used_coupons
 
-# Create app
-app = create_app()
+class TestCoupons(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
-# Manually push an application context
-with app.app_context():
-    used_coupons = fetch_used_coupons()
-    print(used_coupons)
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_fetch_used_coupons(self):
+        used_coupons = fetch_used_coupons()
+        self.assertIsInstance(used_coupons, list)
+
+if __name__ == '__main__':
+    unittest.main()
--- test_mail.py
+++ test_mail.py
@@ -1,10 +1,29 @@
+import unittest
 from app import create_app
 from app.email_services import send_email
 
-# Create app
-app = create_app()
 
-# Manually push an application context
-with app.app_context():
-    coupons = [{'id': '1', 'timestamp': '2022-12-01T00:00:00Z'}, {'id': '2', 'timestamp': '2022-12-01T00:01:00Z'}]
-    send_email('Test Email', 'blockbits30@gmail.com', {"anthony.teixeira55@gmail.com": coupons})
+class TestEmailService(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_send_email(self):
+        self.assertIsNotNone(self.app.config.get('MAIL_USERNAME'))
+        self.assertIsNotNone(self.app.config.get('MAIL_PASSWORD'))
+
+        subject = 'Test Email'
+        sender = self.app.config.get('MAIL_USERNAME')
+        coupons = [{'id': 'COUPON123', 'timestamp': '2021-12-31T23:59:59Z'}]
+        recipients_data = {"anthony.teixeira55@gmail.com": coupons}
+
+        with self.app.app_context():  
+           response = send_email(subject, sender, recipients_data)
+           self.assertIsNone(response)
+
+if __name__ == '__main__':
+    unittest.main()
